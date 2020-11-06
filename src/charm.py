8c809e092dd75308ce64c81ee2d27d43a22bdb3a
Justin Clark <justinmauriceclark@gmail.com>
2020-11-06 13:21:41 -0500

    Basic pod configuration (#2)
    * boilerplate commit
    
    * add basic config file with port option
    
    * add basic metadata.yaml
    
    * add basic pod-spec configuration

--- src/charm.py
+++ src/charm.py
@@ -3,12 +3,15 @@
 # See LICENSE file for licensing details.
 
 import logging
+import textwrap
 
 from ops.charm import CharmBase
 from ops.main import main
+from ops.model import ActiveStatus, MaintenanceStatus
 from ops.framework import StoredState
 
 logger = logging.getLogger(__name__)
+DOCKER_IMAGE = 'graylog/graylog:3.3.8-1'
 
 
 class GraylogCharm(CharmBase):
@@ -17,12 +20,51 @@ class GraylogCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.stop, self._on_stop)
 
     def _on_config_changed(self, _):
-        current = self.model.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+        self._configure_pod()
+
+    def _on_stop(self, event):
+        self.unit.status = MaintenanceStatus('Pod is terminating')
+
+    def _build_pod_spec(self):
+        config = self.model.config
+        spec = {
+            'version': 3,
+            'containers': [{
+                'name': self.app.name,  # self.app.name is defined in metadata.yaml
+                'imageDetails': {
+                    'imagePath': DOCKER_IMAGE
+                },
+                'ports': [{
+                    'containerPort': config['port'],
+                    'protocol': 'TCP'
+                }],
+                'volumeConfig': [{
+                    'name': 'config',
+                    'mountPath': '/etc/graylog/server',
+                    'files': [{
+                        'path': 'server.conf',
+                        'content': textwrap.dedent("""\
+                            is_master = true
+                            password_secret = admin
+                            """)
+                    }],
+                }],
+            }]
+        }
+        return spec
+
+    def _configure_pod(self):
+        """Configure the K8s pod spec for Graylog."""
+        if not self.unit.is_leader():
+            self.unit.status = ActiveStatus()
+            return
+
+        spec = self._build_pod_spec()
+        self.model.pod.set_spec(spec)
+        self.unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
