c953c03fe3ecf519ecdeae15c218aac1e5b5bcce
justin <justinmauriceclark@gmail.com>
2020-11-06 10:51:41 -0500

    add basic pod-spec configuration

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -7,9 +7,25 @@ from unittest.mock import Mock
 from ops.testing import Harness
 from charm import GraylogCharm
 
+BASE_CONFIG = {
+    'port': 9000
+}
+
 
 class TestCharm(unittest.TestCase):
     def setUp(self) -> None:
-        harness = Harness(GraylogOperatorCharm)
-        self.addCleanup(harness.cleanup)
-        harness.begin()
+        self.harness = Harness(GraylogCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def test_pod_spec_for_leader(self):
+        self.harness.set_leader(True)
+        self.harness.update_config(BASE_CONFIG)
+        spec = self.harness.charm._build_pod_spec()
+        expected_port = 9000
+        actual_port = spec['containers'][0]['ports'][0]['containerPort']
+        self.assertEqual(expected_port, actual_port)
+
+
+    def test_pod_spec_for_non_leader(self):
+        self.harness.set_leader(False)
