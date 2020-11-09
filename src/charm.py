d3fdc2b3f34cdd5b31d0e224912c5e33a5947c4b
Jose C. Massón <jose.masson@canonical.coop>
2020-11-09 17:23:54 -0300

    Reformat files with black

--- src/charm.py
+++ src/charm.py
@@ -21,25 +21,27 @@ class GraylogCharm(CharmBase):
         super().__init__(*args)
 
         # initialize image resource
-        self.image = OCIImageResource(self, 'graylog-image')
+        self.image = OCIImageResource(self, "graylog-image")
 
         # event observations
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.stop, self._on_stop)
         self.framework.observe(
-            self.on['elasticsearch'].relation_changed,
-            self._on_elasticsearch_relation_changed
+            self.on["elasticsearch"].relation_changed,
+            self._on_elasticsearch_relation_changed,
         )
         self.framework.observe(
-            self.on['elasticsearch'].relation_broken,
-            self._on_elasticsearch_relation_broken
+            self.on["elasticsearch"].relation_broken,
+            self._on_elasticsearch_relation_broken,
         )
 
         # initialized stored variables
         # TODO: test whether we can just pass the {ingress-address}:{port} string
         #       from Elasticsearch to Graylog or if we need to send multiple hosts
         #       Hypothesis: just the ingress is fine
-        self._stored.set_default(elasticsearch_uri=str)  # connection str for elasticsearch
+        self._stored.set_default(
+            elasticsearch_uri=str
+        )  # connection str for elasticsearch
 
     @property
     def has_elasticsearch(self):
@@ -52,7 +54,7 @@ class GraylogCharm(CharmBase):
         self._configure_pod()
 
     def _on_stop(self, _):
-        self.unit.status = MaintenanceStatus('Pod is terminating.')
+        self.unit.status = MaintenanceStatus("Pod is terminating.")
 
     def _on_elasticsearch_relation_changed(self, event):
         """Get the relation data from the relation and save to stored variable."""
@@ -61,14 +63,14 @@ class GraylogCharm(CharmBase):
             return
 
         data = event.relation.data[event.unit]
-        ingress_address = data.get('ingress-address')
-        port = data.get('port')
+        ingress_address = data.get("ingress-address")
+        port = data.get("port")
         if ingress_address is None or port is None:
-            logger.warning('No port or ingress-address in Elasticsearch relation data')
+            logger.warning("No port or ingress-address in Elasticsearch relation data")
             return
 
         # if we have the data we need, get the information
-        self._stored.elasticsearch_uri = 'http://{}:{}'.format(
+        self._stored.elasticsearch_uri = "http://{}:{}".format(
             ingress_address,
             port,
         )
@@ -88,36 +90,41 @@ class GraylogCharm(CharmBase):
         try:
             image_info = self.image.fetch()
         except OCIImageResourceError:
-            logging.exception('An error occurred while fetching the image info')
-            self.unit.status = BlockedStatus('Error fetching image information')
+            logging.exception("An error occurred while fetching the image info")
+            self.unit.status = BlockedStatus("Error fetching image information")
             return {}
 
         # baseline pod spec
         spec = {
-            'version': 3,
-            'containers': [{
-                'name': self.app.name,  # self.app.name is defined in metadata.yaml
-                'imageDetails': image_info,
-                'ports': [{
-                    'containerPort': config['port'],
-                    'protocol': 'TCP'
-                }],
-                'volumeConfig': [{
-                    'name': 'config',
-                    'mountPath': '/etc/graylog/server',
-                    'files': [{
-                        'path': 'server.conf',
-                        'content': textwrap.dedent("""\
+            "version": 3,
+            "containers": [
+                {
+                    "name": self.app.name,  # self.app.name is defined in metadata.yaml
+                    "imageDetails": image_info,
+                    "ports": [{"containerPort": config["port"], "protocol": "TCP"}],
+                    "volumeConfig": [
+                        {
+                            "name": "config",
+                            "mountPath": "/etc/graylog/server",
+                            "files": [
+                                {
+                                    "path": "server.conf",
+                                    "content": textwrap.dedent(
+                                        """\
                             is_master = true
                             password_secret = {}
                             elasticsearch_hosts = {}
                             """.format(
-                                config['admin-password'],
-                                self._stored.elasticsearch_uri,
-                        ))
-                    }],
-                }],
-            }]
+                                            config["admin-password"],
+                                            self._stored.elasticsearch_uri,
+                                        )
+                                    ),
+                                }
+                            ],
+                        }
+                    ],
+                }
+            ],
         }
 
         return spec
--- tests/test_charm.py
+++ tests/test_charm.py
@@ -8,8 +8,8 @@ from ops.testing import Harness
 from charm import GraylogCharm
 
 BASE_CONFIG = {
-    'port': 9000,
-    'admin-password': 'admin',
+    "port": 9000,
+    "admin-password": "admin",
 }
 
 
@@ -18,7 +18,7 @@ class TestCharm(unittest.TestCase):
         self.harness = Harness(GraylogCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
-        self.harness.add_oci_resource('graylog-image')
+        self.harness.add_oci_resource("graylog-image")
 
     def test_pod_spec_port(self):
         self.harness.set_leader(True)
@@ -26,7 +26,7 @@ class TestCharm(unittest.TestCase):
         self.harness.charm.on.config_changed.emit()
         spec, _ = self.harness.get_pod_spec()
         expected_port = 9000
-        actual_port = spec['containers'][0]['ports'][0]['containerPort']
+        actual_port = spec["containers"][0]["ports"][0]["containerPort"]
         self.assertEqual(expected_port, actual_port)
 
     def test_elasticsearch_uri(self):
@@ -34,19 +34,19 @@ class TestCharm(unittest.TestCase):
         self.harness.update_config(BASE_CONFIG)
 
         # add the elasticsearch relation
-        rel_id = self.harness.add_relation('elasticsearch', 'elasticsearch')
-        self.harness.add_relation_unit(rel_id, 'elasticsearch/0')
-        rel = self.harness.model.get_relation('elasticsearch')
+        rel_id = self.harness.add_relation("elasticsearch", "elasticsearch")
+        self.harness.add_relation_unit(rel_id, "elasticsearch/0")
+        rel = self.harness.model.get_relation("elasticsearch")
         # add data to the unit
         rel_data = {
-            'ingress-address': '10.183.1.2',
-            'port': 9200,
+            "ingress-address": "10.183.1.2",
+            "port": 9200,
         }
-        self.harness.update_relation_data(rel_id, 'elasticsearch/0', rel_data)
+        self.harness.update_relation_data(rel_id, "elasticsearch/0", rel_data)
         self.assertTrue(self.harness.charm.has_elasticsearch)
 
         # test that elasticsearch-uri properly made it to the _stored variable
-        expected_uri = 'http://10.183.1.2:9200'
+        expected_uri = "http://10.183.1.2:9200"
         self.assertEqual(expected_uri, self.harness.charm._stored.elasticsearch_uri)
 
         # now emit the relation broken event and make sure the _stored variable is cleared
@@ -59,23 +59,27 @@ class TestCharm(unittest.TestCase):
 
         # add the elasticsearch relation
         # TODO: add mongodb when the relation is added
-        es_rel_id = self.harness.add_relation('elasticsearch', 'elasticsearch')
-        self.harness.add_relation_unit(es_rel_id, 'elasticsearch/0')
+        es_rel_id = self.harness.add_relation("elasticsearch", "elasticsearch")
+        self.harness.add_relation_unit(es_rel_id, "elasticsearch/0")
 
         # add data to the unit
         es_rel_data = {
-            'ingress-address': '10.183.9.8',
-            'port': 9200,
+            "ingress-address": "10.183.9.8",
+            "port": 9200,
         }
-        self.harness.update_relation_data(es_rel_id, 'elasticsearch/0', es_rel_data)
+        self.harness.update_relation_data(es_rel_id, "elasticsearch/0", es_rel_data)
         self.assertTrue(self.harness.charm.has_elasticsearch)
 
         # test that the server.conf file contains the correct information
-        expected_server_conf = textwrap.dedent("""\
+        expected_server_conf = textwrap.dedent(
+            """\
             is_master = true
             password_secret = admin
             elasticsearch_hosts = http://10.183.9.8:9200
-            """)
+            """
+        )
         spec, _ = self.harness.get_pod_spec()
-        actual_server_conf = spec['containers'][0]['volumeConfig'][0]['files'][0]['content']
+        actual_server_conf = spec["containers"][0]["volumeConfig"][0]["files"][0][
+            "content"
+        ]
         self.assertEqual(expected_server_conf, actual_server_conf)
