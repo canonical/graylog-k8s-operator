313dbdb1ffddbc923df5ec4284c6af251417b2c8
Jeremy Lounder <jeremy.lounder@canonical.com>
2021-06-03 08:57:33 -0700

    Draft 2 - some typos, fixed formatting, added some new functionality.

--- src/charm.py
+++ src/charm.py
@@ -14,6 +14,7 @@ from ops.framework import StoredState
 
 logger = logging.getLogger(__name__)
 
+
 class GraylogCharm(CharmBase):
     _stored = StoredState()
 
@@ -21,40 +22,65 @@ class GraylogCharm(CharmBase):
         super().__init__(*args)
         self.container = self.unit.get_container("graylog")
 
+        self._stored.set_default("pebble_ready", False)
+
         # event observations
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.stop, self._on_stop)
+        self.framework.observe(self.on.pebble_ready, self._on_pebble_ready)
+        # self.framework.observe(self.on.stop, self._on_stop)
 
         self.framework.observer(
             self.on["graylog-peers"].relation_joined, self._on_peer_relation_joined
         )
 
-        self.provider = GraylogProvider(self, "graylog", "graylog", "3.2")
+        self.provider = GraylogProvider(self, "graylog", "graylog", "3.3.8")
 
-        self.mongodb = MongoDbProvider(self, "mongodb")
-        self.framework.observe(self.mongodb.on["ready"], self._requirement_ready)
+        self.mongodb_lib = MongoDbConsumer(self, "mongodb")
+        self.framework.observe(
+            self.mongodb_lib.on["available"], self._requirement_ready
+        )
 
-        self.elastic = ElasticSearchProvider(self, "elasticsearch", ">=5.0 <7")
-        self.framework.observe(self.elastic.on["ready"], self._requirement_ready)
+        self.elastic_lib = ElasticSearchConsumer(self, "elasticsearch", ">=5.0 <7")
+        self.framework.observe(
+            self.elastic_lib.on["available"], self._requirement_ready
+        )
 
-        #self.model.get_binding(PEER).network.ingress_address,
+        # self.model.get_binding(PEER).network.ingress_address,
+
+    @property
+    def port(self):
+        return str(9000)
+        # return self.config.get("port", 9000)
+
+    @property
+    def is_primary(self):
+        return (
+            self.model.get_relation("graylog-peers")
+            .data[self.app]
+            .get("graylog-primary", "")
+            == self.unit.name
+        )
 
     def _on_config_changed(self, event):
         self._update_peers()
+        self.provider.set_port(self.port)
+        self._configure_pod()
+
+    def _on_pebble_ready(self, event):
+        self._stored.pebble_ready = True
         self._configure_pod()
 
     def _on_stop(self, _):
-        self.unit.status = MaintenanceStatus('Pod is terminating.')
-
+        self.unit.status = MaintenanceStatus("Pod is terminating.")
 
     def _requirement_ready(self, event):
         self._configure_pod()
 
     def _update_peers(self):
-        peers_data = self.model.get_relation("graylog-peers").data[self.app]
         if self.unit.is_leader():
-            if not peers_data["graylog_master"]:
-                peers_data["graylog_master"] = self.unit.name
+            peers_data = self.model.get_relation("graylog-peers").data[self.app]
+            if not peers_data["graylog_primary"]:
+                peers_data["graylog_primary"] = self.unit.name
 
             if not peers_data["password_secret"]:
                 peers_data["password_secret"] = self._generate_secret()
@@ -67,6 +93,19 @@ class GraylogCharm(CharmBase):
             self.model.get_binding(event.relation).network.ingress_address
         )
 
+    def _get_cluster_info(self):
+        relation = self.framework.model.get_relation("graylog-peers")
+        addrs = []
+        for unit in relation.units:
+            if relation.data[unit].private_address:
+                addrs.append(
+                    "http://{}:{}/".format(
+                        relation.data[unit].private_address, self.port
+                    )
+                )
+
+        return addrs
+
     def _generate_secret(self, n=96):
         """The secret of size n used to encrypt/salt the Graylog password
 
@@ -75,7 +114,7 @@ class GraylogCharm(CharmBase):
         # TODO: is this how we want to generate random strings?
         # generate a random secret that will be used for the life of this charm
         chars = string.ascii_letters + string.digits
-        secret = ''.join(random.choice(chars) for _ in range(n))
+        secret = "".join(random.choice(chars) for _ in range(n))
 
         return secret
 
@@ -86,31 +125,30 @@ class GraylogCharm(CharmBase):
         """SHA256 hash of the root password"""
         return hashlib.sha256(password).hexdigest()
 
-
     def _build_pebble_layer(self):
-        elastic_addrs = self.elastic.getClusterInfo()
-        mongodb_addrs = self.mongodb.getClusterInfo()
+        elastic_addrs = self.elastic.get_cluster_info()
+        mongodb_addrs = self.mongodb.get_cluster_info()
         peers_data = self.model.get_relation("graylog-peers").data[self.app]
 
         layer = {
-            "summary" : "Graylog Layer",
-            "description" : "Pebble layer configuration for Graylog",
-            "services" : {
-                "graylog" : {
-                    "override" : "replace",
-                    "summary" : "graylog service",
-                    "command" : "graylog",
-                    "startup" : "enabled",
-                    "environment" : {
-                        'GRAYLOG_IS_MASTER': True,
-                        'GRAYLOG_PASSWORD_SECRET': peers_data["password_secret"],
-                        'GRAYLOG_ROOT_PASSWORD_SHA2': self._hash_password(peers_data["admin_password"]),
-                        'GRAYLOG_HTTP_BIND_ADDRESS': self.bind_address,
-                        'GRAYLOG_HTTP_PUBLISH_URI': self.external_uri,
-                        'GRAYLOG_HTTP_EXTERNAL_URI': self.external_uri,
-                        'GRAYLOG_ELASTICSEARCH_HOSTS': elastic_addrs,
-                        'GRAYLOG_ELASTICSEARCH_DISCOVERY_ENABLED': True,
-                        'GRAYLOG_MONGODB_URI': mongodb_addrs,
+            "summary": "Graylog Layer",
+            "description": "Pebble layer configuration for Graylog",
+            "services": {
+                "graylog": {
+                    "override": "replace",
+                    "summary": "graylog service",
+                    "command": "graylog",
+                    "startup": "enabled",
+                    "environment": {
+                        "GRAYLOG_IS_MASTER": self.is_primary,
+                        "GRAYLOG_PASSWORD_SECRET": peers_data["password_secret"],
+                        "GRAYLOG_ROOT_PASSWORD_SHA2": self._hash_password(
+                            peers_data["admin_password"]
+                        ),
+                        "GRAYLOG_HTTP_BIND_ADDRESS": "0.0.0.0:{}".format(self.port),
+                        "GRAYLOG_ELASTICSEARCH_HOSTS": elastic_addrs,
+                        "GRAYLOG_ELASTICSEARCH_DISCOVERY_ENABLED": True,
+                        "GRAYLOG_MONGODB_URI": mongodb_addrs,
                     },
                 },
             },
@@ -122,15 +160,23 @@ class GraylogCharm(CharmBase):
         """Configure the Pebble layer for Graylog."""
 
         if not self.mongodb.is_valid():
-            logger.warning('Need both MongoDB and Elasticsearch relation for '
-                           'Graylog to function properly. Blocking.')
-            self.unit.status = BlockedStatus('Missing MongoDB relation')
+            logger.warning(
+                "Need both MongoDB and Elasticsearch relation for "
+                "Graylog to function properly. Blocking."
+            )
+            self.unit.status = BlockedStatus("Missing MongoDB relation")
             return False
 
         if not self.elastic.is_valid():
-            logger.warning('Need both MongoDB and Elasticsearch relation for '
-                           'Graylog to function properly. Blocking.')
-            self.unit.status = BlockedStatus('Missing ElasticSearch relation')
+            logger.warning(
+                "Need both MongoDB and Elasticsearch relation for "
+                "Graylog to function properly. Blocking."
+            )
+            self.unit.status = BlockedStatus("Missing ElasticSearch relation")
+            return False
+
+        if not self._stored.pebble_ready:
+            self.unit.status = MaintenanceStatus("Waiting for Pod startup to complete")
             return False
 
         layer = self._build_pebble_layer()
@@ -139,15 +185,18 @@ class GraylogCharm(CharmBase):
             return False
 
         if not layer.service.graylog.environment.GRAYLOG_ELASTICSEARCH_HOSTS:
-            self.unit.status = MaintenanceStatus('Related ElasticSearch not yet ready.')
+            self.unit.status = MaintenanceStatus("Related ElasticSearch not yet ready.")
             return False
 
         if not layer.service.graylog.environment.GRAYLOG_MONGODB_URI:
-            self.unit.status = MaintenanceStatus('Related MongoDb not yet ready.')
+            self.unit.status = MaintenanceStatus("Related MongoDb not yet ready.")
             return False
 
         self.container.add_layer("graylog", layer, combine=True)
+        self.container.autostart()
+        self.provider.ready()
         self.unit.status = ActiveStatus()
+        return True
 
 
 if __name__ == "__main__":
--- src/graylog_provider.py
+++ src/graylog_provider.py
@@ -3,30 +3,32 @@ import logging
 from ops.charm import CharmEvents
 from ops.framework import StoredState, EventSource, EventBase
 from ops.relation import ProviderBase
+
 logger = logging.getLogger(__name__)
 
+
 class GraylogProvider(ProviderBase):
-
-
     def __init__(self, charm, name, service, version=None):
         super().__init__(charm, name, service, version)
 
         self.charm = charm
 
-        self.framework.observe(self.charm.on[self.name].relation_joined,
-                               self._on_relation_joined)
-        self.framework.observe(self.charm.on[self.name].relation_changed,
-                               self._on_relation_changed)
-        self.framework.observe(self.charm.on[self.name].relation_broken,
-                               self._on_relation_broken)
+        self.framework.observe(
+            self.charm.on[self.name].relation_joined, self._on_relation_joined
+        )
+        self.framework.observe(
+            self.charm.on[self.name].relation_changed, self._on_relation_changed
+        )
+        self.framework.observe(
+            self.charm.on[self.name].relation_broken, self._on_relation_broken
+        )
 
     @property
     def unit(self):
         return self.charm.unit
 
-
     def _on_relation_joined(self, event):
-        event.relation.data[self.unit].private_address = str(
+        event.relation.data[self.unit].public_address = str(
             self.model.get_binding(event.relation).network.ingress_address
         )
 
@@ -34,9 +36,8 @@ class GraylogProvider(ProviderBase):
         if self.model.unit.is_leader():
             logger.debug("Notifying Consumer : %s", data)
             for rel in self.framework.model.relations[self.name]:
-                rel.data[self.model.app]['graylog_port'] = port
+                rel.data[self.model.app]["graylog_port"] = port
 
-    #def _on_relation_changed(self, event):
+    # def _on_relation_changed(self, event):
 
-
-    #def _on_relation_broken(self, event):
+    # def _on_relation_broken(self, event):
